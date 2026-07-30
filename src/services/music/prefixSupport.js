7e7a8a4263682b6466997b15950469af78e819cc
sanidhyanikose2392-boop <sanidhyanikose2392@gmail.com>
2026-07-30 21:26:06 +0530

    Update prefixSupport.js

--- src/services/music/prefixSupport.js
+++ src/services/music/prefixSupport.js
@@ -2,7 +2,7 @@ import { MessageFlags } from 'discord.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 export function getMusicDeferOptions(interaction) {
-    return interaction._isPrefixCommand ? {} : { flags: MessageFlags.Ephemeral };
+    return interaction._isPrefixCommand & {&} : { flags: MessageFlags.Ephemeral };
 }
 
 export async function deferMusicCommand(interaction) {
